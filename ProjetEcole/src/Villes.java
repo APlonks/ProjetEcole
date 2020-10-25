bbb52d37f72c7b761ae0376fa14dc4135340e320
wynyy <waypoirier@gmail.com>
2020-10-25 16:04:59 +0100

    ﻿Fermeture du scanner et suppression des variable non utiliser.

--- ProjetEcole/src/Villes.java
+++ ProjetEcole/src/Villes.java
@@ -6,17 +6,16 @@ public class Villes {
 		int nombreVilles = 0;
 		Scanner scan= new Scanner(System.in);
 		do {
-			System.out.println("Entrez le nombre de villes de la communauté d'agglomération\n");
+			System.out.println("Entrez le nombre de villes de la communaute d'agglomeration\n");
 			nombreVilles = scan.nextInt();	
 		}
 		while(nombreVilles < 0 || nombreVilles > 26);
 		System.out.println("Vous avez choisi " + nombreVilles + " Villes\n");
 		
-		int option = 0;
-		System.out.println("Sélectionner une option :");
+		System.out.println("Selectionner une option :");
 		System.out.println("1) ajouter une route;\n");
 		System.out.println("2) fin.\n");
-		
+		scan.close();
 	}
 
 }
