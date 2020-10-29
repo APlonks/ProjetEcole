ce3fc1775c6c809cc48b900826e6696541677e65
wynyy <waypoirier@gmail.com>
2020-10-29 17:48:56 +0100

    Ajout de la gestion des String et Int a Scan. Scan resiste mieux aux entrer utilisateur eronne. Ajout d'ereur a Scan avec ScanException.

--- ProjetEcole/src/Scan.java
+++ ProjetEcole/src/Scan.java
@@ -1,40 +1,149 @@
 import java.util.Scanner;
+import java.lang.StringBuilder;
 
 /**
- * Classe static et final du scanner. Sera utilis� pour faire les entrers claviers.
- * @param s Scanner de la class.
+ * Classe static et final du scanner. Sera utiliser pour faire les entrers claviers.
+ * @param s (Scanner) Scanner de la class.
+ * @param ligne (StringBuilder) Entrer utilisateur que l'on traite.
  */
 public class Scan {
 	private static final Scanner s = new Scanner(System.in);
+	private static StringBuilder ligne = new StringBuilder("");
+
 	/**
-	 * @return Scanner.next();
+	 * Update ligne.
 	 */
-	public static String lireString() {
-		return s.next();
+	private static void updateLigne() throws ScanException, java.util.InputMismatchException {
+		ligne.setLength(0);
+		try {
+			ligne.append(s.nextLine());
+		} catch(Exception e) {
+			throw new ScanException("Entrer vide");
+		}
+		retireEspc();
 	}
 	/**
-	 * @return Scanner.nextInt();
+	 * Retire les espace en debut de ligne.
 	 */
-	public static int lireInt() {
-		return s.nextInt();
-	}
-	/**
-	 * @return Scanner.nextLine();
-	 */
-	public static String lireLigne() {
-		return s.nextLine();
-	}
-	/**
-	 * Si System.in n'est pas vide, on affiche a l'utilisateur que ces valeurs seront ignorer. */
-	public static void ignoreSiExist() {
-		if (s.hasNext()) {
-			System.out.println("L'entrer "+s.nextLine()+" est ignorer.");
+	private static void retireEspc() {
+		while (!estVide() && ligne.charAt(0)==' ') {
+			ligne.delete(0,1);
 		}
 	}
 	/**
-	 * Scanner.close();
+	 * On retire de ligne se qui est considerer comme la premiere entre utilisateur.
 	 */
-	public static void fermer() {
-	s.close();
+	private static void deleteFirtInput() {
+		while (!estVide() && ligne.charAt(0)!=' ') {
+			ligne.delete(0,1);
+		}
+		retireEspc();
+	}
+
+	/**
+	 * Permet de savoir si le buffer de l'entrer utilisateur est vide.
+	 */
+	public static boolean estVide() {
+		if (ligne.length() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	/**
+	 * Realise une nouvelle entrer utilisateur.
+	 * @return Le premier mot de l'entrer realiser.
+	 */
+	public static String lireMot() throws ScanException {
+		/* Mise a jour de l'entrer utilisateur. */
+		updateLigne();
+		String retour;
+		int fin = ligne.indexOf(" ");
+		if (estVide()) {
+			throw new ScanException("Entrer vide");
+		} else if (fin != -1) {
+			/* Il y a un espace dans la ligne. */
+			retour = ligne.subSequence(0,fin).toString();
+		} else {
+			/* On retourne l'entrer entiere qui est constituer d'un seul mot. */
+			retour = ligne.toString();
+		}
+		/* Retire l'entrer que l'on vient de retourner. */
+		deleteFirtInput();
+		return retour;
+	}
+	/**
+	 * @return Lis le premier mot qu'il reste dans le buffer ligne.
+	 */
+	public static String motSuivant() throws ScanException {
+		if (estVide()) {
+			/* Ereur, il n'y as plus rien a lire. */
+			throw new ScanException("Plus rien a lire");
+		}
+		String retour;
+		int fin = ligne.indexOf(" ");
+		if (fin != -1) {
+			retour = ligne.subSequence(0,fin).toString();
+		} else {
+			retour = ligne.toString();
+		}
+		/* Retire de la ligne se que l'on vient vas retourner. */
+		deleteFirtInput();
+		return retour;
+	}
+	/**
+	 * Realise une nouvelle entrer utilisateur.
+	 * @return La valeur de l'entier taper par l'utilisateur.
+	 */
+	public static int lireEntier() throws ScanException {
+		String mot;
+		int retour=0;
+		char lettre;
+		try {
+			/* Utilise lireMot pour obtenir la premiere entrer utilisateur. */
+			mot = lireMot();
+		} catch (ScanException e) {
+			/* On récupére une ereur dans la fonction lireMot(), on l'a transmet. */
+			throw e;
+		}
+		/* Lettre de l'entier taper.
+		 * Si jamais un des symbole du mot n'est pas un chiffre, on retourne une ereur.
+		 */
+		for (int i=0; i<mot.length(); i++) {
+			lettre = mot.charAt(i);
+			if (lettre<'0' || lettre>'9') {
+				throw new ScanException("L'entrer n'est pas un entier");
+			} else {
+				retour = retour*10+lettre-'0';
+			}
+		}
+		return retour;
+	}
+	/**
+	 * @return La valeur de l'entier suivant dans le buffer ligne.
+	 */
+	public static int entierSuivant() throws ScanException {
+		String mot;
+		int retour=0;
+		char lettre;
+		try {
+			/* Utilise motSuivant pour obtenir l'entrer utilisateur suivante. */
+			mot = motSuivant();
+		} catch (ScanException e) {
+			/* On récupére une ereur dans la fonction entierSuivant(), on l'a transmet. */
+			throw e;
+		}
+		/* Lettre de l'entier taper.
+		 * Si jamais un des symbole du mot n'est pas un chiffre, on retourne une ereur.
+		 */
+		for (int i=0; i<mot.length(); i++) {
+			lettre = mot.charAt(i);
+			if (lettre<'0' || lettre>'9') {
+				throw new ScanException("L'entrer n'est pas un entier");
+			} else {
+				retour = retour*10+lettre-'0';
+			}
+		}
+		return retour;
 	}
 }
---
+++ ProjetEcole/src/ScanException.java
@@ -0,0 +1,14 @@
+/**
+ * Exception de la class Scan.
+ * @param ereur Message d'ereur.
+ */
+public class ScanException extends Exception {
+	private String ereur;
+
+	/**
+	 * Constructeur.
+	 */
+	public ScanException(String msg) {
+		super(msg);
+	}
+}
