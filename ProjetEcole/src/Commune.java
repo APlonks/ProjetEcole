74c09927848120e5085e6425bb92b654d77204e4
APlonks <antoine.planque@gmail.com>
2020-11-04 08:20:08 +0100

    Ajout des méthodes AddRoute, AddRouteInversee, SupprimerRoute, SupprimmerRouteIversee et afficheRoute,la condition des ecoles n'est pas implémentée.

--- ProjetEcole/src/Commune.java
+++ ProjetEcole/src/Commune.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
  * @param nombresVilles Nombres de villes.
  */
 public class Commune {
-	private static HashMap<Agglomeration,ArrayList<Agglomeration>> commune;
+	private HashMap<Agglomeration,ArrayList<Agglomeration>> commune;
 	private int nombresVilles;
 
 	/**
@@ -38,14 +38,14 @@ public class Commune {
 	/**
 	 * Affiche la liste des villes (en ligne).
 	 */
-	public static void affiche() {
+	public void affiche() {
 		for (Agglomeration k : commune.keySet()) {
 			System.out.print(k+" ");
 		}
 		System.out.print("\n");
 	}
 	
-	public static void afficheEcole() {
+	public void afficheEcole() {
 		for (Agglomeration k : commune.keySet()) {
 			if (k.getEcole() == true) {
 				System.out.print(k+" ");
@@ -65,4 +65,83 @@ public class Commune {
 		}
 		return null;
 	}
+	
+	/**
+	 * Ajoute une route entre deux villes (de x vers y).
+	 * @param x La Ville de départ
+	 * @param y La ville d'arrivée 
+	 */
+	public void addRoute(String x, String y) {
+		Agglomeration depart=getVille(x);//On récupère la ville de départ.
+		Agglomeration arrivee = getVille(y);//On récupère la ville d'arrivée.
+		//if(!commune.containsValue(arrivee))//Vérifier si l'ArrayList de la ville ne contient pas la ville d'arrivee, j'arrive pas à y accéder
+			if(commune.containsKey(depart)) {//On vérifie si la commune la ville de départ existe.
+				commune.get(depart).add(arrivee);
+			}
+			else {
+				System.out.println("La ville "+x+" n'existe pas.");
+			}
+			System.out.println("L'ArrayList de la ville "+x+" pour check :"+commune.get(depart));//Simplement pour retourner l'ArrayList de la ville de départ à la fin de la fonction pour tester, on peut l'enlever
+	}
+	
+	/**
+	 * Ajoute la route inverse entre deux villes (de y vers x).
+	 * @param x La Ville de départ
+	 * @param y La ville d'arrivée 
+	 */
+	public void addRouteInversee(String x, String y) {
+		Agglomeration depart=getVille(x);//On récupère la ville de départ
+		Agglomeration arrivee = getVille(y);//On récupère la ville d'arrivée
+		//if(!commune.containsValue(depart));//Vérifier si l'ArrayList de la ville de départ ne contient pas la ville de départ, j'arrive pas à y accéder
+			if(commune.containsKey(arrivee)) {//On vérifie si dans la commune la ville d'arrivée existe
+				commune.get(arrivee).add(depart);
+			}
+			else {
+				System.out.println("La ville "+y+" n'existe pas.");
+			}
+		System.out.println("L'ArrayList de la ville "+y+" pour check :"+commune.get(arrivee));//Simplement pour retourner l'ArrayList de la ville de arrivee à la fin de la fonction pour tester, on peut l'enlever
+	}
+	
+	public void supprimerRoute(String x, String y) {
+		Agglomeration depart=getVille(x);//On récupère la ville de départ.
+		Agglomeration arrivee = getVille(y);//On récupère la ville d'arrivée.
+		if(commune.containsKey(depart)) {//On vérifie si la commune la ville de départ existe.
+			//if(commune.containsValue(d'arrivée))//Verifier si l'ArrayList detient la ville d'arrivée avant de la supprimée mais je n'arrive pas à y accèder
+				commune.get(depart).remove(arrivee);
+		}
+		else {
+			System.out.println("La ville "+x+" n'existe pas.");
+		}
+		System.out.println("L'ArrayList de la ville "+x+" pour check :"+commune.get(depart));//Simplement pour retourner l'ArrayList de la ville de départ à la fin de la fonction pour tester, on peut l'enlever
+	}
+	
+	public void supprimerRouteInverse(String x, String y) {
+		Agglomeration depart=getVille(x);//On récupère la ville de départ.
+		Agglomeration arrivee = getVille(y);//On récupère la ville d'arrivée.
+		if(commune.containsKey(arrivee)) {//On vérifie si la commune la ville de départ existe.
+			//if(commune.containsValue(depart))Vérifier si l'ArrayList détient la ville de départ avant de la supprimée mais je n'arrive pas à y accéder
+				commune.get(arrivee).remove(depart);
+		}
+		else {
+			System.out.println("La ville "+y+" n'existe pas.");
+		}
+		System.out.println("L'ArrayList de la ville "+y+" pour check :"+commune.get(arrivee));//Simplement pour retourner l'ArrayList de la ville de arrivee à la fin de la fonction pour tester, on peut l'enlever
+	}
+	
+	/**
+	 * Affiche les routes liées à une ville.//Elle n'est pas impllémentée
+	 * @param x la ville choisie
+	 */
+	public void afficheRoute(String x) {
+		Agglomeration depart=getVille(x);//On récupère la ville
+		if(commune.containsKey(depart)) {//On vérifie si dans la commune la ville d'arrivée existe
+			System.out.println(commune.remove(depart));
+		}
+		else
+			System.out.println("La ville "+x+" n'existe pas.");
+	}
+	
+	
+	
+	
 }
