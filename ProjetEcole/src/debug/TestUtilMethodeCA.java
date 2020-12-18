72d9846597cdc9fdad0f4699a3e6fef421039d07
Pinto <Pinto@Pinto>
2020-12-18 12:11:01 +0100

    Modifs Junit

--- ProjetEcole/src/debug/TestUtilMethodeCA.java
+++ ProjetEcole/src/debug/TestUtilMethodeCA.java
@@ -2,15 +2,9 @@ package debug;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import principal.CA;
-import principal.Ville;
-import principal.CAException;
 import principal.UtilMethodeCA;
-import java.util.HashMap;
+
 
 public class TestUtilMethodeCA {
 	@Test
--- ProjetEcole/src/debug/VilleTest.java
+++ ProjetEcole/src/debug/VilleTest.java
@@ -2,36 +2,41 @@ package debug;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import principal.Ville;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class VilleTest {
+	private Ville ville1,ville2,ville3;
+	
+	@BeforeAll
+	void beforeAll() {
+		ville1 = new Ville("Paris", true);
+		ville2 = new Ville("Paris", false);
+		ville3 = new Ville("P", true);
+	}
 
 	@Test
 	void testNomVille() {
-		Ville test = new Ville("Paris");
-		assertEquals("Paris", test.getNomVille());
+		assertEquals("Paris", ville1.getNomVille());
 	}
 	
 	@Test
 	void testEcoleVille() {
-		Ville test = new Ville("Paris", true);
-		assertTrue(test.getEcole());
+		assertTrue(ville1.getEcole());
 	}
 	
 	@Test
 	void testCompareToVille() {
-		Ville ville1 = new Ville("Paris");
-		Ville ville2 = new Ville("Paris");
 		assertNotEquals(1, ville2.getNomVille().compareTo(ville1.getNomVille()));
 	}
 	
 	@Test
 	void testEquals() {
-		Ville ville1 = new Ville("Paris");
-		Ville ville2 = new Ville("Paris");
-		Ville ville3 = new Ville("P");
 		assertTrue(ville1.equals(ville2));
 		assertFalse(ville3.equals(ville1));
 	}
