b3cd6a144aeb6a97619851112c9b5ea16631f6be
Pinto <Pinto@Pinto>
2020-12-18 11:18:32 +0100

    Junit

---
+++ ProjetEcole/src/debug/TestUtilMethodeCA.java
@@ -0,0 +1,24 @@
+package debug;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import principal.CA;
+import principal.Ville;
+import principal.CAException;
+import principal.UtilMethodeCA;
+import java.util.HashMap;
+
+public class TestUtilMethodeCA {
+	@Test
+	void testNomAutomatique() {
+		assertEquals("E", UtilMethodeCA.nomAutomatique(5));
+		assertEquals("AB", UtilMethodeCA.nomAutomatique(28));
+		assertEquals("KN", UtilMethodeCA.nomAutomatique(300));
+	}
+	
+}
---
+++ ProjetEcole/src/debug/VilleTest.java
@@ -0,0 +1,14 @@
+package debug;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VilleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
